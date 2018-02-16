4769405f8fffcbe91d8201fc234f6746e463f284
Kristina <kristinaplauche@10.0.0.7>
2018-02-15 21:00:01 -0500

    Game class correction

---
+++ src/cityofaaron/control/CropControl.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cityofaaron.control;
+import cityofaaron.model.CropData;
+/**
+ *
+ * @author kristinaplauche
+ */
+import java.util.Random;
+public class CropControl {
+    
+   
+ private static Random random = new Random();
+    
+    // The calcEatenByRats method
+// Purpose: To determine wheat eaten by rats
+// Parameters: none
+// Returns: the number of bushels eaten by rats
+// Pre-conditions: wheat in store must be positive
+// and the percentage of offering must be positive
+
+
+    public static int calcEatenByRats (CropData cropData) {
+        int wheat = cropData.getWheatInStore();
+        int offering = cropData.getOffering();
+        
+        // if wheatInStore < 0, return -1
+        if (wheat < 0)
+            return -1;
+  // if offering < 0 , return -1  
+ // if offering > 100, return -1
+ if (offering < 0 || offering > 100)
+      return -1;
+ 
+ // if random number < 30, then calculate: 
+ int randomRats = random.nextInt(101);
+ int eatenByRats = 0;
+ 
+ 
+ if (randomRats < 30) {
+     
+     
+ // if offering is < 8%, multiply wheat * random integer between 6% and 10%
+ if (offering < 8) {
+     eatenByRats = wheat * (random.nextInt(5)+6)/100;
+ }
+ 
+ // if offering is between < 8% and 12% inclusive, multiply wheat * random value between 3% and 7%.
+ if (offering >= 8 && offering <= 12) {
+     eatenByRats = wheat * (random.nextInt(5)+3)/100;
+ }
+ 
+ // if offering is > 12%, multiply wheat * random value between 3% and  5%
+ if (offering > 12) {
+     eatenByRats  = wheat *  (random.nextInt(3)+3)/100;
+ }
+ // wheatInStore = wheatInStore - eatenByRats
+ wheat -= eatenByRats;
+ cropData.setWheatInStore(wheat);
+ 
+ // return bushels eatenByRats
+ return eatenByRats;
+
+}
+ 
+ 
+
+    
+   
+    
+
